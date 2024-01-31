2a014dc0cb9267fa123889d4e4505a5c04e4b56b
Cezar Silva <cezarfelipedasilva@gmail.com>
2024-01-31 20:16:28 -0300

    Feature/86873wacj/env (#33)
    * Update README.md (#5)
    
    * Update README.md (#6)
    
    * Feature/8686zznj8 estrutura inicial (#9)
    
    * Update README.md (#3)
    
    Co-authored-by: Cezar Silva <cezarfelipedasilva@gmail.com>
    
    * feat: iniciado estrutura base do projeto
    
    * Feature/8686zznj8 estrutura inicial (#7)
    
    * Update README.md (#5)
    
    * Update README.md (#6)
    
    * feat: iniciado estrutura base do projeto
    
    * feat: atualizado diretorio contexto do dockerfile
    
    ---------
    
    Co-authored-by: Cezar Silva <cezarfelipedasilva@gmail.com>
    
    * feat: criado projeto angular e estrutura inicial para rodar a aplicação conforme padrão do projeto
    
    * feat:  implementado header html e css
    
    * feat: implementado footer page default
    
    * feat: alterado titulo page e favicon
    
    * bug: erro no processo de compilação, correção falha teste de compilação
    
    * feat: novo teste de compilação
    
    * feat: incluido arquivo environment ts
    
    * feat: Configuração deploy ambiente kube
    
    * feat: incluido dependencia fluxo deploy
    
    * feat: atualizado path do arquivo deployment yaml
    
    * feat: alterado github action que substitui variavel
    
    * feat: removido linha invalida
    
    * feat: substituição de variavel usando bash sed
    
    * feat: alterado forma de realizar deploy
    
    * feat: teste com kube config no repositorio
    
    * feat: incluído secret github action para as variaveis do deployment.yaml
    
    * feat: removido arquivos utilizado para teste
    
    * feat: erro dno formato do arquivo github action build e deploy
    
    * feat: implementado sobre variavel para atualização da versão imagem docker
    
    * feeat: incluido ambiente no step de Deploy
    
    * feat: incluído configuração do contexto kube config github action
    
    * feat: alterado secrets por variables githubaction
    
    * feat: atualização port container e manisfestos k8s
    
    * feat: atualização docker file app ui
    
    * feat: ajustado margens botão de acesso ao sistema
    
    * feat: criado arquivo nginx conf
    
    * feat: configurado angular, nginx e docker imagem
    
    * feat: estrutura para variaveis de ambiente local e cloud app-ui
    
    * feat: config variaveis pipeline deploy
    
    ---------
    
    Co-authored-by: CezarFelps <156339257+CezarFelps@users.noreply.github.com>

--- app-ui/src/environments/environment.local.ts
+++ app-ui/src/environments/environment.local.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: true,
+  //@ts-ignore
+  ENVIRONMENT: window["env"]["APP_ENVIRONMENT"],
+  //@ts-ignore
+  VERSION: window["env"]["APP_VERSION"],
   APP_API:{
     URL:'${APP_API_URL}',
     AUTH_USER:'${SECURITY_OAUTH_CLIENT_NAME}',
