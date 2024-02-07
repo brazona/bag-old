15e097f29f2822d3a10542b2f79a4d54c9842a10
Cezar Silva <cezarfelipedasilva@gmail.com>
2024-02-06 23:36:45 -0300

    test: build container localhost

--- app-ui/env.sh
+++ app-ui/env.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
+
 NPM_RUN="$1"
-PATH_ENV=../.resources/environments/.env
+ENV_CLOUD="build"
 
 rm -rf ./src/environments/environment.prod.ts
 rm -rf ./src/environments/environment.local.ts
 rm -rf ./src/assets/env.run-time.js
-rm -rf lista_env.txt
-if [ $NPM_RUN = "build" ] 
+
+if [ $NPM_RUN = $ENV_CLOUD ] 
 then
-  echo "Run script Environment Container Docker"
   cp ./src/environments/environment.ts ./src/environments/environment.prod.ts
 else
-  echo "Run script Environment Localhost"
   cp ./src/environments/environment.ts ./src/environments/environment.local.ts
   cp ./src/assets/env.template.js ./src/assets/env.run-time.js
   while read -r line || [[ -n "$line" ]];
@@ -21,7 +20,7 @@ else
     value='${'$varname'}'
     #sed -i 's/'$value'/'$varvalue'/' ./src/environments/environment.local.ts
     sed -i 's/'$value'/'$varvalue'/' ./src/assets/env.run-time.js  
-  done < "$PATH_ENV"
+  done < ../.resources/environments/.env
   envsubst < ./src/assets/env.run-time.js > ./src/assets/env.js
   rm -rf ./src/assets/env.run-time.js
 fi
