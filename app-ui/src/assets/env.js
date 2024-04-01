a5b07964bfccbeac09e10359bf545a22ab6032ac
Cezar Silva <cezarfelipedasilva@gmail.com>
2024-04-01 16:20:50 -0300

    feat: reorganização dos diretorios para inclusão da aplicação de teste dos micro serviços

--- app-ui/src/assets/env.js
+++
@@ -1,6 +0,0 @@
-(function(window) {
-    window.env = window.env || {};
-    // Environment variables
-    window["env"]["APP_ENVIRONMENT"] = "localhost";
-    window["env"]["APP_VERSION"] = "1.0.1";
-  })(this);
