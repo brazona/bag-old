5137db5abb6293f73a52245098bf5e427e935ced
Cezar Silva <cezarfelipedasilva@gmail.com>
2024-04-30 17:47:50 -0300

    feat: atualização do caminho da variavel de ambiente

--- .resources/db/init-docker-compose.sh
+++ .resources/db/init-docker-compose.sh
@@ -1,3 +1,3 @@
 #Execute docker compose with file .env
 docker network create bag-app-net
-docker compose --env-file ../environments/docker.env up -d --build --force-recreate
+docker compose --env-file ../environments/local.env up -d --build --force-recreate
