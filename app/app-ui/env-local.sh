dcf393780cd2784160be70d49d95f144258da3f2
Cezar Silva <cezarfelipedasilva@gmail.com>
2024-04-30 23:58:17 -0300

    feat: atualizado configuração variavel de ambiente app ui

--- app/app-ui/env-local.sh
+++ app/app-ui/env-local.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
 rm -rf ./src/assets/env.run-time.js
+DIRETORIO_ENV=../../.resources/environments/local.env
+DOCKER_ENV=dockercompose
 
 #cp ./src/environments/environment.ts ./src/environments/environment.local.ts
 cp ./src/assets/env.template.js ./src/assets/env.run-time.js
+echo "Valor paramentro: $1"
+echo "Valor variável: $DOCKER_ENV"
+
+if [ $1 = $DOCKER_ENV ]; then
+    echo "ENVIRONMENT: DOCKER COMPOSE"
+    DIRETORIO_ENV=../../.resources/environments/docker.env
+else
+    echo "ENVIRONMENT: LOCAL"
+fi
+sleep 5
 while read -r line || [[ -n "$line" ]];
 do
   varname=$(printf '%s\n' "$line" | sed -e 's/=.*//')
@@ -11,6 +23,7 @@ do
   value='${'$varname'}'
   #sed -i 's/'$value'/'$varvalue'/' ./src/environments/environment.local.ts
   sed -i 's/'$value'/'$varvalue'/' ./src/assets/env.run-time.js  
-done < ../../.resources/environments/.env
+done < $DIRETORIO_ENV
+#done < ../../.resources/environments/local.env
 envsubst < ./src/assets/env.run-time.js > ./src/assets/env.js
 rm -rf ./src/assets/env.run-time.js
--- app/app-ui/src/environments/environment.ts
+++ app/app-ui/src/environments/environment.ts
@@ -4,10 +4,18 @@ export const environment = {
   ENVIRONMENT: window["env"]["APP_ENVIRONMENT"],
   //@ts-ignore
   VERSION: window["env"]["APP_VERSION"],
+  //@ts-ignore
   APP_API:{
-    URL:'${APP_API_URL}',
-    AUTH_USER:'${SECURITY_OAUTH_CLIENT_NAME}',
-    AUTH_PASS:'${SECURITY_OAUTH_CLIENT_PASS}',
-    GRANT: '${APP_API_GRANT}'
+    //@ts-ignore
+    URL: window["env"]["APP_API_URL"],
+    BASIC_AUTH:{
+      //@ts-ignore
+      AUTH_USER: window["env"]["AUTHENTICATION_USER"],
+      //@ts-ignore
+      AUTH_PASS:window["env"]["AUTHENTICATION_PASS"]
+    },
+    
+    //@ts-ignore
+    GRANT: window["env"]["AUTHENTICATION_GRANT"]
   },
 };
