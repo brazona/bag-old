78da4d50dfaf68e32c4a314b7c25a630a0bcf244
Cezar Silva <cezarfelipedasilva@gmail.com>
2024-05-01 21:57:51 -0300

    feat: estrutura html e css inicial pagina login

--- app/app-ui/src/app/pages/login/login.component.ts
+++ app/app-ui/src/app/pages/login/login.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  standalone: true,
+  imports: [MatGridListModule, MatButtonModule],
 })
 export class LoginComponent {
 
